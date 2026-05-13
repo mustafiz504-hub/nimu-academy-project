e8f6952c4ec696edc3f2805ccb1b8be3d24a4afd
Mustafiz ansari <mustafiz504@gmail.com>
2026-05-14 01:42:41 +0530

    chore: connect supabase database and update ssl config

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,10 +5,11 @@ require('dotenv').config();
 /** @type {import('pg').Pool} */
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false,
 });
 
 // Test connection
